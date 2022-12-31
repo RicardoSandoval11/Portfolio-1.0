21f3c13ed1471865504d890554a61070387c86d9
Ricardo <ricardosandoval344@gmail.com>
2022-12-31 08:51:56 -0600

    creating the structure of the first react project

---
+++ React/01-e-commerce-app/src/ECommercerApp.jsx
@@ -0,0 +1,10 @@
+
+
+export const ECommercerApp = () => {
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
---
+++ React/01-e-commerce-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ECommercerApp } from './ECommercerApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ECommercerApp />
+  </React.StrictMode>,
+)
